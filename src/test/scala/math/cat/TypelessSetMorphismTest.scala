b5c080d7e14174f745de9b860f42b1ac5620da5b
Vlad Patryshev <vpatryshev@gmail.com>
2015-07-01 18:24:36 -0700

    All tests pass now. Will work on - warnings - making sure the tests in Scala cover all the stuff covered in Java

--- src/test/scala/math/cat/TypelessSetMorphismTest.scala
+++ src/test/scala/math/cat/TypelessSetMorphismTest.scala
@@ -1,5 +1,7 @@
 package math.cat
 
+import math.cat.Sets._
+import math.cat.TypelessSetMorphism._
 import org.specs2.mutable._
 
 /**
@@ -7,135 +9,148 @@ import org.specs2.mutable._
  */
 
 class TypelessSetMorphismTest extends Specification {
-  /*
+  "TypelessSetMorphism" >> {
 
-  "building TypelessSetMorphism" >> {
-    val sut = TypelessSetMorphism("test", Set(1, 2, "a"), Set("x1", "x2", "xa", 77), (x: Any) => "x" + x);
-    assert(sut(1) == "x1")
-    assert(sut("a") == "xa")
-    try {
-      sut(3)
-      failure("3 is not in domain")
-    } catch {
-      case e: Exception => // praise the Lord!
+    "building TypelessSetMorphism" >> {
+      val sut = TypelessSetMorphism("test", Set(1, 2, "a"), Set("x1", "x2", "xa", 77), (x: Any) => "x" + x)
+      (sut(1) == "x1") must beTrue
+      (sut("a") == "xa") must beTrue
+      try {
+        sut(3)
+        failure("3 is not in domain")
+      } catch {
+        case e: Exception => // praise the Lord!
+      }
+      true
     }
-  }
 
-  "TypelessSetMorphism then another" >> {
-    val x = Set(1, 2, "a")
-    val y = Set("x1", "x2", "xa", 77)
-    val z = Set(2, 28, x)
-    val f = new TypelessSetMorphism("f", x, y, (x: Any) => "x" + x);
-    val g = new TypelessSetMorphism("g", y, z, (y: Any) => y.toString.length)
-    val sut = f then g
-    assert(sut(1) == 2)
-    assert(sut("a") == 2)
-    try {
-      sut(z)
-      failure("3 is not in domain")
-    } catch {
-      case e: Exception => // praise the Lord
+    "TypelessSetMorphism then another" >> {
+      val x = Set(1, 2, "a")
+      val y = Set("x1", "x2", "xa", 77)
+      val z = Set(2, 28, x)
+      val f = new TypelessSetMorphism("f", x, y, (x: Any) => "x" + x)
+      val g = new TypelessSetMorphism("g", y, z, (y: Any) => y.toString.length)
+      val sut = f then g
+      (sut(1) == 2) must beTrue
+      (sut("a") == 2) must beTrue
+      try {
+        sut(z)
+        failure("3 is not in domain")
+      } catch {
+        case e: Exception => // praise the Lord
+      }
+      true
     }
-  }
 
-  "TypelessSetMorphism before another" >> {
-    val x = Set(1, 2, "a")
-    val y = Set("x1", "x2", "xa", 77)
-    val z = Set(2, 28, x)
-    val f = new TypelessSetMorphism("f", x, y, (x: Any) => "x" + x);
-    val g = new TypelessSetMorphism("g", y, z, (y: Any) => y.toString.length)
-    val sut = g before f
-    assert(sut.d0 == x)
-    assert(sut.d1 == z)
-    assert(f(1) == "x1")
-    assert(sut(1) == 2)
-    assert(sut("a") == 2)
-    try {
-      sut(z)
-      failure("3 is not in domain")
-    } catch {
-      case e: Exception => // praise the Lord
+    "TypelessSetMorphism before another" >> {
+      val x = Set(1, 2, "a")
+      val y = Set("x1", "x2", "xa", 77)
+      val z = Set(2, 28, x)
+      val f = new TypelessSetMorphism("f", x, y, (x: Any) => "x" + x)
+      val g = new TypelessSetMorphism("g", y, z, (y: Any) => y.toString.length)
+      val sut = g before f
+      (sut.d0 == x) must beTrue
+      (sut.d1 == z) must beTrue
+      (f(1) == "x1") must beTrue
+      (sut(1) == 2) must beTrue
+      (sut("a") == 2) must beTrue
+      try {
+        sut(z)
+        failure("3 is not in domain")
+      } catch {
+        case e: Exception => // praise the Lord
+      }
+      true
     }
-  }
 
-  "building a constant" >> {
-    val s0 = Set(1, 2, "a")
-    val s1 = Set("x1", "x2", "xa", 77)
-    val sut = TypelessSetMorphism.constant(s0, s1, 77);
-    assert(sut.d0 == s0)
-    assert(sut.d1 == s1)
-    for (x <- s0) assert(sut(x) == 77)
-    try {
-      sut(3)
-      failure("3 is not in domain")
-    } catch {
-      case e: Exception => // praise the Lord!
+    "building a constant" >> {
+      val s0 = Set(1, 2, "a")
+      val s1 = Set("x1", "x2", "xa", 77)
+      val sut = TypelessSetMorphism.constant(s0, s1, 77)
+      (sut.d0 == s0) must beTrue
+      (sut.d1 == s1) must beTrue
+      for (x <- s0) (sut(x) == 77) must beTrue
+      try {
+        sut(3)
+        failure("3 is not in domain")
+      } catch {
+        case e: Exception => // praise the Lord!
+      }
+      true
     }
-  }
 
-  "building a nonexistent constant" >> {
-    try {
-      val sut = TypelessSetMorphism.constant(Set(1, 2, "a"), Set("x1", "x2", "xa", 77), "xx");
-      failure("xx is not in codomain")
-    } catch {
-      case e: Exception => // praise the Lord!
+    "building a nonexistent constant" >> {
+      try {
+        val sut = TypelessSetMorphism.constant(Set(1, 2, "a"), Set("x1", "x2", "xa", 77), "xx")
+        failure("xx is not in codomain")
+      } catch {
+        case e: Exception => // praise the Lord!
+      }
+      true
     }
-  }
 
-  "building an inclusion" >> {
-    val s0 = Set(1, 2, "a")
-    val s1 = Set(0, 1, 2, "b", s0, "a")
-    val sut = inclusion(s0, s1);
-    assert(sut.d0 == s0)
-    assert(sut.d1 == s1)
-    for (x <- s0) assert(sut(x) == x)
-    try {
-      sut("b")
-    } catch  {
-      case e: Exception => // Hallelujah!
+    "building an inclusion" >> {
+      val s0 = Set(1, 2, "a")
+      val s1 = Set(0, 1, 2, "b", s0, "a")
+      val sut = inclusion(s0, s1)
+      (sut.d0 == s0) must beTrue
+      (sut.d1 == s1) must beTrue
+      for (x <- s0) (sut(x) == x) must beTrue
+      try {
+        sut("b")
+      } catch {
+        case e: Exception => // Hallelujah!
+      }
+      true
     }
-  }
 
-  "building a predicate-based inclusion" >> {
-    val s = Set(1, 2, 77, 90, 42, "1xya2")
-    def predicate = (x:Any) => x.toString.apply(0) == '1'
-    val sut = inclusion(s, predicate);
-    assert(sut.d1 == s)
-    assert(sut.d0 == (s filter predicate))
-    for (x <- List(1, "1xya2")) assert(sut(x) == x)
-    try {
-      sut(2)
-    } catch  {
-      case e: Exception => // Hallelujah!
+    "building a predicate-based inclusion" >> {
+      val s = Set(1, 2, 77, 90, 42, "1xya2")
+      def predicate = (x: Any) => x.toString().charAt(0) == '1'
+
+      val sut = inclusion(s, predicate)
+      (sut.d1 == s) must beTrue
+      (sut.d0 == (s filter predicate)) must beTrue
+      for (x <- List(1, "1xya2")) (sut(x) == x) must beTrue
+      try {
+        sut(2)
+        failure("Must have thrown an exception")
+      } catch {
+        case e: Exception => // Hallelujah!
+      }
+      true
     }
-  }
 
-  "building a unit" >> {
-    val s = Set(1, 2, "a")
-    val sut = unit(s);
-    assert(sut.d0 == s)
-    assert(sut.d1 == s)
-    for (x <- s) assert(sut(x) == x)
-    try {
-      sut("b")
-    } catch  {
-      case e: Exception => // Hallelujah!
+    "building a unit" >> {
+      val s = Set(1, 2, "a")
+      val sut = unit(s)
+      (sut.d0 == s) must beTrue
+      (sut.d1 == s) must beTrue
+      for (x <- s) (sut(x) == x) must beTrue
+      try {
+        sut("b")
+        failure("Should have thrown an exception")
+      } catch {
+        case e: Exception => // Hallelujah!
+      }
+      true
     }
+
+    "for factorset" >> {
+      val set0: collection.Set[Int] = setOf(1 to 10)
+      val set = set0.map(i => i:Any)
+      def isOdd(x: Any) = x.toString.charAt(0) % 2 == 0
+      val br: BinaryRelationship[Any, Any] = ((a: Any, b: Any) => isOdd(a) == isOdd(b))
+      val factoring = new FactorSet[Any](set, br)
+      val s = Array(Set(2, 4, 6, 8), Set(1, 3, 5, 7, 9, 10))
+      val sut = forFactorset(factoring)
+      val factor = Set(s(1), s(0))
+      (set == sut.d0) must beTrue
+      (factor == sut.d1) must beTrue
+      (s(0) == sut(8)) must beTrue
+      (s(1) == sut(5)) must beTrue
+    }
+
   }
 
-  "for factorset" >> {
-    val set:Set[Any] = setOf(1 to 10)
-    def isOdd(x: Any) = x.toString.charAt(0) % 2 == 0
-    val br: BinaryRelationship[Any, Any] = ((a: Any, b: Any) => isOdd(a) == isOdd(b))
-    val factoring = new FactorSet(set, br)
-    val s = Array(Set(2, 4, 6, 8), Set(1, 3, 5, 7, 9, 10))
-    val sut = forFactorset(factoring)
-    val factor = Set(s(1), s(0))
-    assert(set == sut.d0, "got " + sut.d0)
-    assert(factor == sut.d1, "got " + sut.d1)
-    assert(s(0) == sut(8))
-    assert(s(1) == sut(5))
-  }
-
-  */
 }
