6b0dd19b76da950dccc149664c663cb3495c5334
Vlad Patryshev <vpatryshev@gmail.com>
2018-12-14 12:43:35 -0800

    One more testcase added for the diagram

--- src/test/scala/math/cat/SetDiagramTest.scala
+++ src/test/scala/math/cat/SetDiagramTest.scala
@@ -10,20 +10,7 @@ import org.specs2.mutable._
 class SetDiagramTest extends Specification {
 
   "SetDiagram" should {
-    "get validated" in {
-      val a: set = Set(1, 2, 3, 4, 5)
-      val b: set = Set(0, 1, 2)
-      val f = SetFunction("f", a, b, x => Math.min(2, x.toString.toInt))
-      val g = SetFunction("g", b, b, x => x.toString.toInt % 3)
-      SetDiagram(
-        "ParallelPair", Category.ParallelPair,
-        Map("0" -> a, "1" -> b),
-        Map("a" -> f, "b" -> g)
-      ) must throwA(new IllegalArgumentException(
-        "requirement failed: Inconsistent mapping for d0(b)"))
-      ok
-    }
-    "have a limit" in {
+    "get validated - positive" in {
       val a: set = Set(1, 2, 3)
       val b: set = Set(2, 3, 4)
       val c: set = Set(0, 1)
@@ -36,6 +23,66 @@ class SetDiagramTest extends Specification {
       )
       sut.domain === Category.Pullback
       sut.codomain === Setf
+    }
+
+    "get validated - negative" in {
+      val a: set = Set(1, 2, 3, 4, 5)
+      val b: set = Set(0, 1, 2)
+      val f = SetFunction("f", a, b, x => Math.min(2, x.toString.toInt))
+      val g = SetFunction("g", b, b, x => x.toString.toInt % 3)
+      SetDiagram(
+        "ParallelPair", Category.ParallelPair,
+        Map("0" -> a, "1" -> b),
+        Map("a" -> f, "b" -> g)
+      ) must throwA(new IllegalArgumentException(
+        "requirement failed: Inconsistent mapping for d0(b)"))
+      ok
+    }
+    
+    "have a limit of empty diagram" in {
+      val sut = SetDiagram(
+        "terminal", Category._0_,
+        Map(),
+        Map()
+      )
+      sut.domain === Category._0_
+      sut.codomain === Setf
+      sut.limit match {
+        case None => failure("We expected a limit")
+        case Some(sut.Cone(vertex, arrowTo)) =>
+          vertex.size === 1
+      }
+      ok
+    }
+
+    "have a limit of a point" in {
+      val x: set = Set("x", "y", "z")
+      val sut = SetDiagram(
+        "terminal", Category._1_,
+        Map(0 -> x),
+        Map()
+      )
+      sut.domain === Category._1_
+      sut.codomain === Setf
+      sut.limit match {
+        case None => failure("We expected a limit")
+        case Some(sut.Cone(vertex, arrowTo)) =>
+          vertex.size === x.size
+      }
+      ok
+    }
+    
+    "have a limit that is a pullback" in {
+      val a: set = Set(1, 2, 3)
+      val b: set = Set(2, 3, 4)
+      val c: set = Set(0, 1)
+      val ac = SetFunction("f", a, c, _.toString.toInt % 2)
+      val bc = SetFunction("g", b, c, x => (x.toString.toInt + 1) % 2)
+      val sut = SetDiagram(
+        "pullback", Category.Pullback,
+        Map("a" -> a, "b" -> b, "c" -> c),
+        Map("ac" -> ac, "bc" -> bc)
+      )
       sut.limit match {
         case None => failure("We expected a limit")
         case Some(sut.Cone(vertex, arrowTo)) =>
@@ -54,6 +101,7 @@ class SetDiagramTest extends Specification {
       }
       ok
     }
+
     "have a limit that is an equalizer" in {
       val a: set = Set(1, 2, 3, 4, 5)
       val b: set = Set(0, 1, 2)
@@ -64,8 +112,6 @@ class SetDiagramTest extends Specification {
         Map("0" -> a, "1" -> b),
         Map("a" -> f, "b" -> g)
       )
-      sut.domain === Category.ParallelPair
-      sut.codomain === Setf
       sut.limit match {
         case None => failure("We expected a limit")
         case Some(sut.Cone(vertex, arrowTo)) =>
