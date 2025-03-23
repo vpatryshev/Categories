7215454731803780bd7e40f1e434795a06f4337f
Vlad Patryshev <vpatryshev@gmail.com>
2025-03-23 09:30:15 +0100

    WIP. tests improved

--- scala3/src/test/scala/math/cat/topos/GrothendieckToposTest.scala
+++ scala3/src/test/scala/math/cat/topos/GrothendieckToposTest.scala
@@ -1,147 +1,71 @@
 package math.cat.topos
 
-import math.Test
-import math.cat.Categories.*
 import math.cat.topos.CategoryOfDiagrams.DiagramArrow
-import math.cat.{Category, SetFunction}
-import math.sets.Sets
-import math.sets.Sets.{itsaset, set}
+import math.cat.SetFunction
 import org.specs2.matcher.MatchResult
-import scalakittens.Result.*
 
 import scala.language.postfixOps
 import SetFunction.*
 
 class GrothendieckToposTest extends Fixtures:
 
-  "Subobject classifier" should {
-    "exist for`𝟘`" in:
-      val topos = `Set^𝟘`
-      import topos._
-      val points = Ω.points
-      points.size === 1
+  def checkPoints(topos: GrothendieckTopos, expected: String*): MatchResult[Any] =
+    val points = topos.Ω.points
+    points.map(_.toShortString.replaceFirst("p\\d+\\(", "").dropRight(1)) === expected.toList
 
-    "exist for`𝟙`" in {
-      val topos = `Set^𝟙`
-      import topos._
-      val points = Ω.points
-      points.size === 2
-      val omega0 = Ω("0").toList
-      omega0 match
-        case omega00 :: omega01 :: Nil =>
-          omega00("0").isEmpty must beTrue
-          omega01("0").isEmpty must beFalse
-        case bad => failure(s"Expected 2 points in $bad")
+  "Subobject classifier" should:
+    "be good for`𝟘`" in:
+      checkPoints(`Set^𝟘`, "")
 
-      points.map(_.toShortString) === List("p0(0->())", "p1(0->(0->{0.0}))")
-    }
+    "be good for`𝟙`" in:
+      checkPoints(`Set^𝟙`, "0->()", "0->(0->{0.0})")
 
-    "exist for _2_" in {
-      val topos = `Set^_2_`
-      import topos._
-      val points = Ω.points
-      val omega0 = Ω("0")
-      omega0.size === 3
-      val omega1 = Ω("1")
-      omega1.size === 2
-      points.map(_.toShortString) === 
-        "p0(0->(), 1->())"::"p1(0->(1->{0.1}), 1->(1->{1.1}))"::"p2(0->(0->{0.0}, 1->{0.1}), 1->(1->{1.1}))"::Nil
-    }
+    "be good for _2_" in:
+      checkPoints(`Set^_2_`,
+        "0->(), 1->()",
+        "0->(1->{0.1}), 1->(1->{1.1})",
+        "0->(0->{0.0}, 1->{0.1}), 1->(1->{1.1})")
 
-    "exist for _3_" in {
-      val topos = `Set^_3_`
-      import topos._
-      val points = Ω.points
-      val omega0 = Ω("0")
-      omega0.size === 4
-      val omega1 = Ω("1")
-      omega1.size === 3
-      points.map(_.toShortString) ===
-        "p0(0->(), 1->(), 2->())"::
-        "p1(0->(2->{0.2}), 1->(2->{1.2}), 2->(2->{2.2}))"::
-        "p2(0->(1->{0.1}, 2->{0.2}), 1->(1->{1.1}, 2->{1.2}), 2->(2->{2.2}))"::
-        "p3(0->(0->{0.0}, 1->{0.1}, 2->{0.2}), 1->(1->{1.1}, 2->{1.2}), 2->(2->{2.2}))"::Nil
-    }
+    "be good for _3_" in:
+      checkPoints(`Set^_3_`,
+        "0->(), 1->(), 2->()",
+        "0->(2->{0.2}), 1->(2->{1.2}), 2->(2->{2.2})",
+        "0->(1->{0.1}, 2->{0.2}), 1->(1->{1.1}, 2->{1.2}), 2->(2->{2.2})",
+        "0->(0->{0.0}, 1->{0.1}, 2->{0.2}), 1->(1->{1.1}, 2->{1.2}), 2->(2->{2.2})")
+      
+    "be good for ParallelPair" in:
+      checkPoints(`Set^ParallelPair`,
+      "0->(), 1->()",
+      "0->(1->{a,b}), 1->(1->{1})",
+      "0->(0->{0}, 1->{a,b}), 1->(1->{1})")
 
-    "exist for ParallelPair" in {
-      val topos = `Set^ParallelPair`
-      import topos._
-      val points = Ω.points
-      val expected =
-        "p0(0->(), 1->())" ::
-        "p1(0->(1->{a,b}), 1->(1->{1}))" ::
-        "p2(0->(0->{0}, 1->{a,b}), 1->(1->{1}))" ::
-        Nil
-      points.map(_.toShortString) === expected
-    }
+    "be good for Pullback" in:
+      checkPoints(`Set^Pullback`,
+        "a->(), b->(), c->()",
+        "a->(c->{ac}), b->(c->{bc}), c->(c->{c})",
+        "a->(c->{ac}), b->(b->{b}, c->{bc}), c->(c->{c})",
+        "a->(a->{a}, c->{ac}), b->(c->{bc}), c->(c->{c})",
+        "a->(a->{a}, c->{ac}), b->(b->{b}, c->{bc}), c->(c->{c})")
 
-    "exist for Pullback" in:
-      val topos = `Set^Pullback`
-      import topos._
-      val points = Ω.points
-      val expected =
-        "p0(a->(), b->(), c->())"::
-          "p1(a->(c->{ac}), b->(c->{bc}), c->(c->{c}))"::
-          "p2(a->(c->{ac}), b->(b->{b}, c->{bc}), c->(c->{c}))"::
-          "p3(a->(a->{a}, c->{ac}), b->(c->{bc}), c->(c->{c}))"::
-          "p4(a->(a->{a}, c->{ac}), b->(b->{b}, c->{bc}), c->(c->{c}))"::
-          Nil
+    "be good for Pushout" in:
+      checkPoints(`Set^Pushout`,
+        "a->(), b->(), c->()",
+        "a->(c->{ac}), b->(), c->(c->{c})",
+        "a->(b->{ab}), b->(b->{b}), c->()",
+        "a->(b->{ab}, c->{ac}), b->(b->{b}), c->(c->{c})",
+        "a->(a->{a}, b->{ab}, c->{ac}), b->(b->{b}), c->(c->{c})"
+      )
 
-      points.size === expected.size
-      val actual = points.map(_.toShortString)
+    "be good for Z3" in:
+      checkPoints(`Set^Z3`, "0->()","0->(0->{0,1,2})")
 
-      (actual zip expected) foreach { case (a, e) => a must_== e }
+  "Classifying map" should:
 
-      points.map(_.toShortString) === expected
-
-    "exist for Pushout" in {
-      val topos = `Set^Pushout`
-      import topos._
-
-      val actualMappings = Ω.objMappings.map(_.toShortString).sorted
-
-      val points = Ω.points
-      val actual = points.map(_.toShortString)
-
-      val expected = 
-        "p0(a->(), b->(), c->())" ::
-        "p1(a->(c->{ac}), b->(), c->(c->{c}))" ::
-        "p2(a->(b->{ab}), b->(b->{b}), c->())" ::
-        "p3(a->(b->{ab}, c->{ac}), b->(b->{b}), c->(c->{c}))" ::
-        "p4(a->(a->{a}, b->{ab}, c->{ac}), b->(b->{b}), c->(c->{c}))" ::
-        Nil
-
-      points.size === expected.size
-
-      (actual zip expected) foreach { case (a, e) => a must_== e }
-      points.map(_.toShortString) === expected
-    }
-
-    "exist for Z3" in {
-      val topos = `Set^Z3`
-      import topos._
-      val points = Ω.points
-      points.map(_.toShortString) === "p0(0->())"::"p1(0->(0->{0,1,2}))"::Nil
-    }
-  }
-
-  "Classifying map" should {
-
-    "exist for ParallelPair" in {
+    "be good for ParallelPair" in:
       val topos = `Set^ParallelPair`
       import topos._
       val omega = topos.Ω
 
-      try
-        val aboutOmega = omega.toString
-        println(aboutOmega)
-        ok
-      catch
-        case e: Throwable =>
-          System.err.println(e)
-          e.printStackTrace()
-          ok
-          
       val i1: topos.Arrow = inclusionOf(SampleParallelPairSubdiagram1) in SampleParallelPairDiagram1.asOldDiagram iHope
       val χ1: DiagramArrow = topos.χ(i1)
       val χ10: SetFunction = asFunction(χ1("0"))
@@ -155,6 +79,4 @@ class GrothendieckToposTest extends Fixtures:
       short(χ11(1)) === "Diagram[ParallelPair](1->{1})"
       short(χ11(2)) === "Diagram[ParallelPair](1->{1})"
       short(χ11(3)) === "Diagram[ParallelPair]()"
-    }
-  }
 
--- scala3/src/test/scala/math/cat/topos/TopologyTest.scala
+++ scala3/src/test/scala/math/cat/topos/TopologyTest.scala
@@ -1,39 +1,52 @@
 package math.cat.topos
 
-import math.cat.Categories._
+import math.cat.Categories.*
 import math.cat.topos.CategoryOfDiagrams.DiagramArrow
-import scalakittens.{Good, Result}
+import scalakittens.Result.Oops
+import scalakittens.{Bad, Good, Result}
 
 import scala.language.reflectiveCalls
 
 class TopologyTest extends Fixtures:
 
-  def topologiesTested(cat: Cat): List[Result[LawvereTopology]] =
-    val topos = new CategoryOfDiagrams(cat)
-    val Ω = topos.Ω.asOldDiagram
-    val subs: List[Diagram] = Ω.subobjects.toList
+  def topologiesTested(topos: CategoryOfDiagrams): List[Result[LawvereTopology]] =
     import topos._
+    val subs: List[topos.Diagramme] = Ω.subobjects.toList
 
     val inclusionsToΩ =
-      subs map (topos.inclusionOf(_) in Ω) collect { case Good(incl) => incl }
+      subs map { sub =>
+        try {
+          val supbold = sub.asOldDiagram
+          val incl = topos.inclusionOf(supbold) in Ω.asOldDiagram
+          incl
+        } catch {
+          case e: Exception =>
+            println(s"Failed on $sub")
+            e.printStackTrace()
+            Oops(e)
+        }
+      } collect { case Good(incl) => incl }
 
     val builder = LawvereTopology.forPredicate(topos)
 
-    val predicates = inclusionsToΩ map topos.predicateForArrowToΩ
+    val predicates = inclusionsToΩ map {
+      d =>
+        topos.predicateForArrowToΩ(d)
+    }
 
     predicates map builder
 
-  def topologies(cat: Cat): List[LawvereTopology] =
-    topologiesTested(cat) collect { case Good(topo) => topo}
+  def topologies(topos: CategoryOfDiagrams): List[LawvereTopology] =
+    topologiesTested(topos) collect { case Good(topo) => topo}
 
   "Topologies" should {
-    "exist for `𝟘`" in {
-      val all = topologies(`𝟘`)
+    "exist for`𝟘`" in {
+      val all = topologies(`Set^𝟘`)
       all.length === 1
     }
 
-    "exist for `𝟙`" in {
-      val candidates = topologiesTested(`𝟙`)
+    "exist for`𝟙`" in {
+      val candidates = topologiesTested(`Set^𝟙`)
       candidates.size === 4
 
       expectOk(candidates(3))
@@ -43,31 +56,31 @@ class TopologyTest extends Fixtures:
     }
 
     "exist for _2_" in {
-      topologies(_2_).size === 4
+      topologies(`Set^_2_`).size === 4
     }
 
     "exist for _3_" in {
-      topologies(_3_).size === 8
+      topologies(`Set^_3_`).size === 8
     }
 
     "exist for ParallelPair" in {
-      topologies(ParallelPair).size === 4
+      topologies(`Set^ParallelPair`).size === 4
     }
 
     "exist for Pullback" in {
-      topologies(Pullback).size === 8
+      topologies(`Set^Pullback`).size === 8
     }
 
     "exist for Pushout" in {
-      topologies(Pushout).size === 8
+      topologies(`Set^Pushout`).size === 8
     }
 
     "exist for Z3" in {
-      topologies(Z3).size === 2
+      topologies(`Set^Z3`).size === 2
     }
 
     "exist for HalfSimplicial" in {
-      topologies(Simplicial3).size === 6
+      topologies(`Set^Simplicial`).size === 6
     }
 
   }
